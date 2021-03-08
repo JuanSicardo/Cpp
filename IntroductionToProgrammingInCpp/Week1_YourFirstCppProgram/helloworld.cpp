07289263d1e129cbd07eeca52ae251d980ada6f3
Juan Antonio Sicardo Contreras <contacto@juansicardo.com>
2021-03-08 16:34:37 -0600

    Added Week1

---
+++ IntroductionToProgrammingInCpp/Week1_YourFirstCppProgram/helloworld.cpp
@@ -0,0 +1,12 @@
+/*Author: Juan Antonio Sicardo Contreras
+* This program prints "Hello World" to the console
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
---
+++ IntroductionToProgrammingInCpp/Week1_YourFirstCppProgram/sum.cpp
@@ -0,0 +1,22 @@
+/*From the "Introduction to Programming in C++" course material, at NYUx
+* This program reads two integers from the user and prints their sum
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int num1;	//will hold the first input
+	int num2;	//will hold the second input
+	int sum;	//will hold the sum
+
+	cout << "Please enter two numbers separated by a space:" << endl;
+	cin >> num1 >> num2;
+
+	sum = num1 + num2;
+
+	cout << num1 << " + " << num2 << " = " << sum << endl;
+
+	return 0;
+}
